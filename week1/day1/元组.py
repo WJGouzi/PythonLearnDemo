5b023235fc52ff15db5e91b5c18ccef0c5c582ae
gouzi <>
2018-01-22 22:55:19 +0800

    元组

---
+++ week1/day1/元组.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author name__ = 'gouzi'
+__create time__ = '2018/1/22'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻━━━┓
+            ┃    ☃    ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃    ┻     ┃
+            ┗━┓      ┏━┛
+              ┃      ┗━━━━━━━┓
+              ┃   神兽保佑    ┣┓
+              ┃    永无BUG！ ┏┛
+              ┗┓┓┏━━┳━┓ ┏━━━┛
+               ┃┫┫    ┃┫┫
+               ┗┻┛    ┗┻┛
+"""
+tup1 = () # 用 '()' 进行创建 其余的和列表几乎是一样的。
+tup2 = (1,) # 如果是只有一个元素的元组的话，最好要加上一个 ',' 为了以后的扩展的方便进行使用的。
+
+a = (1, 3, 4, 5)
+print(a[1:4])
+
+# print(a[3] = 5) ##### 这里会报错，因为是元组是不能进行修改的，所以如果有一些不能修改的数据列表 推荐使用元组。
+
+
+
