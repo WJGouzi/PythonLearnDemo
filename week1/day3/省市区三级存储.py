3f66e9eba695725ff0172515cb3f09146c044510
gouzi <>
2018-01-24 23:52:10 +0800

    省市区三级存储功能

---
+++ week1/day3/省市区三级存储.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author name__ = 'gouzi'
+__create time__ = '2018/1/24'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻━━━┓
+            ┃    ☃    ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃    ┻     ┃
+            ┗━┓      ┏━┛
+              ┃      ┗━━━━━━━┓
+              ┃   神兽保佑    ┣┓
+              ┃    永无BUG！ ┏┛
+              ┗┓┓┏━━┳━┓ ┏━━━┛
+               ┃┫┫    ┃┫┫
+               ┗┻┛    ┗┻┛
+"""
+
+
+print('省市区三级菜单'.center(50, '-'))
+citys = {'四川':{
+                '成都': {
+                        '青羊区':{},
+                        '锦江区':{},
+                        '武侯区':{},
+                        '双流区':{}
+                        },
+                '德阳': {
+                        '旌阳区':{},
+                        '中江县':{},
+                        '罗江区':{},
+                        '绵竹市':{},
+                        '什邡市':{},
+                        '广汉市':{}
+                        },
+                },
+         '福建':{
+                '福州': {
+                        '福州区':{},
+                        '桥东区':{},
+                        '哈哈去':{}
+                        },
+                '漳州': {
+                        '漳浦':{},
+                        '诏安':{}
+                        }
+                }
+        }
+
+
+cityFile = open('citys','w+')
+cityFile.write(str(citys))
+cityFile.seek(0)
+cityDict = eval(cityFile.read())
+parentList = []
+addList = [] # 增加的添加在列表中。
+while True:
+    for key in cityDict:
+        print(key)
+    choose = input('>>>:').strip()
+    if len(choose) == 0: continue
+    if choose in cityDict:
+        parentList.append(cityDict)
+        cityDict = cityDict[choose]
+    elif choose == 'quit':
+        cityDict = parentList.pop() if len(parentList) != 0 else citys
+        # break
+    elif choose == 'add': # 增加的功能
+        addCity = input('addCity>>>:').strip()
+        cityDict[addCity] = {}
+        addList.append(cityDict)
+        cityFile.seek(0)
+        cityFile.write(str(addList[0]))
+        cityFile.flush()
+        print(addList)
+    else:
+        print('输入不正确')
+
+
+cityFile.close()
