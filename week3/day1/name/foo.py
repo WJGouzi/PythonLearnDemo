19f6914ab9248ea320cb2922c6204b06963cc03d
wangjun <1693841903@qq.com>
2018-02-05 17:43:55 +0800

    __name__和'__main__'

---
+++ week3/day1/name/foo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: wj
+@license: (C) Copyright 2013-2018.
+@contact: 1693841903@qq.com
+@file: foo.py
+@version: 1.0
+@time: 2018/2/5 10:33
+@desc:
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓     ┏┓
+            ┏┛┻━━━━━┛┻┓
+            ┃    ☃    ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃    ┻     ┃
+            ┗━┓     ┏━━┛
+              ┃     ┗━━━━┓
+              ┃  神兽保佑  ┣┓
+              ┃　永无BUG！  ┏┛
+              ┗┓┓┏━━━━━┳┓┏┛
+               ┃┫┫     ┃┫┫
+               ┗┻┛     ┗┻┛
+"""
+
+def hello():
+    print('hello')
+    print(__name__)
+
+
+if __name__ == '__main__':
+    hello()
+# 当前的文件的时候，只有自己的逻辑代码的时候，才写在这里面，其他的文件调用的时候，是调用不到的。 一定要加上、
+
