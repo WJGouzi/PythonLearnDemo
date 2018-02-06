9bc6a70e863be55f4cc7fa3f680511bc6c69d169
wangjun <1693841903@qq.com>
2018-02-06 17:13:26 +0800

    itertools的demo

--- week3/day2/testDemo.py
+++ week3/day2/testDemo.py
@@ -1,12 +1,13 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 @author: wj
 @license: (C) Copyright 2013-2018.
 @contact: 1693841903@qq.com
-@file: main.py
+@file: testDemo.py
 @version: 1.0
-@time: 2018/2/5 09:37
+@time: 2018/2/6 14:13
 @desc:
 # code is far away from bugs with the god animal protecting
     I love animals. They taste delicious.
@@ -23,14 +24,27 @@
                ┃┫┫     ┃┫┫
                ┗┻┛     ┗┻┛
 """
-import sys
-import os
+import itertools
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-print(os.path.dirname(__file__))
+def pi(N):
+    ' 计算pi的值 '
+    # step 1: 创建一个奇数序列: 1, 3, 5, 7, 9, ...
+    oddListGenerator = itertools.count(1, 2)
+    # step 2: 取该序列的前N项: 1, 3, 5, 7, 9, ..., 2*N-1.
+    oddList = list(itertools.takewhile(lambda x: x <= 2*N-1, oddListGenerator))
+    sum = 0
+    for i in oddList:
+        # step 3: 添加正负符号并用4除: 4/1, -4/3, 4/5, -4/7, 4/9, ...
+        # step 4: 求和:
+        sum += float(4/i) * (-1)**oddList.index(i)
+    return sum
 
-from module import logger
 
-def main():
-    logger.logging()
 
+
+print(pi(10000))
+assert 3.04 < pi(10) < 3.05
+assert 3.13 < pi(100) < 3.14
+assert 3.140 < pi(1000) < 3.141
+assert 3.1414 < pi(10000) < 3.1415
+print('ok')
